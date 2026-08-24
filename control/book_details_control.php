0f4cc60dd342a442334ccb44ca159ff9f0891fae
mdshihabshahriar <shihabshahriar922@gmail.com>
2026-08-25 00:07:16 +0600

    book details control implemented

---
+++ control/book_details_control.php
@@ -0,0 +1,35 @@
+<?php
+
+include "../model/db.php";
+
+$mydb = new mydb();
+
+$conobj = $mydb->openConn();
+
+
+if (isset($_GET["id"])) {
+
+    $id = $_GET["id"];
+
+    $result = $mydb->getBookById(
+        $conobj,
+        $id
+    );
+
+    if ($result->num_rows > 0) {
+
+        $book = $result->fetch_assoc();
+
+    } else {
+
+        die("Book not found.");
+
+    }
+
+} else {
+
+    die("Book ID is missing.");
+
+}
+
+?>
