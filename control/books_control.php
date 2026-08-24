4c76707811dbf9d4b8367120cad1bcbca2bf276d
mdshihabshahriar <shihabshahriar922@gmail.com>
2026-08-25 00:01:14 +0600

    books_control added

---
+++ control/books_control.php
@@ -0,0 +1,34 @@
+<?php
+
+include "../model/db.php";
+
+$mydb = new mydb();
+
+$conobj = $mydb->openConn();
+
+
+// Search
+if (isset($_GET["search"]) && !empty($_GET["search"])) {
+
+    $search = $_GET["search"];
+
+    if (isset($_GET["filter"])) {
+        $filter = $_GET["filter"];
+    } else {
+        $filter = "title";
+    }
+
+    $result = $mydb->searchBooks(
+        $conobj,
+        $search,
+        $filter
+    );
+
+
+// Show all books
+} else {
+
+    $result = $mydb->getAllBooks($conobj);
+}
+
+?>
